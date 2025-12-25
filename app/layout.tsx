794ac441a6b8f13c0a9901895fddeac5e7ba3312
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2025-12-25 17:38:24 +0530

    style(theme): configure Geist sans and mono fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { ThemeToggle } from "./components/ThemeToggle";
+import { ThemeToggle } from "@/app/components/ThemeToggle";
 
 export default function Home() {
   return (
     <div className="text-2xl">
       <ThemeToggle />
-      Hello World
+      <p>Hello World</p>
+      <p className="font-mono">Hello World</p>
+      <p className="font-mono">$ 200</p>
     </div>
   );
 }
